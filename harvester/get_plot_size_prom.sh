2844494e2203a0f76d37f63fabd739485f92f706
neurosis69 <83925572+neurosis69@users.noreply.github.com>
2021-09-28 11:17:51 +0200

    Create get_plot_size_prom.sh

---
+++ harvester/get_plot_size_prom.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+NFT_PLOT_LIST=`curl -s --insecure --cert ~/.chia/mainnet/config/ssl/harvester/private_harvester.crt --key ~/.chia/mainnet/config/ssl/harvester/private_harvester.key -d '{}' -H "Content-Type: application/json" -X POST https://localhost:8560/get_plots | jq '.plots[] | select(.pool_contract_puzzle_hash!=null) | .file_size'`
+OG_PLOT_LIST=`curl -s --insecure --cert ~/.chia/mainnet/config/ssl/harvester/private_harvester.crt --key ~/.chia/mainnet/config/ssl/harvester/private_harvester.key -d '{}' -H "Content-Type: application/json" -X POST https://localhost:8560/get_plots | jq '.plots[] | select(.pool_public_key!=null) | .file_size'`
+export NFT_SUM=OG_SUM=0
+for i in $NFT_PLOT_LIST; do NFT_SUM=$((NFT_SUM + i)); done
+for i in $OG_PLOT_LIST; do OG_SUM=$((OG_SUM + i)); done
+echo -e "# HELP chia_og_plots_size_bytes size of all OG plots in bytes"
+echo -e "# TYPE chia_og_plots_size_bytes gauge"
+echo -e "chia_og_plots_size_bytes "$OG_SUM
+echo -e "# HELP chia_nft_plots_size_bytes size of all OG plots in bytes"
+echo -e "# TYPE chia_nft_plots_size_bytes gauge"
+echo -e "chia_nft_plots_size_bytes "$NFT_SUM
