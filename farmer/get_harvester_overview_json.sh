6d2e7f9329c80ac36a3925599ad9940e9fae04c0
neurosis69 <83925572+neurosis69@users.noreply.github.com>
2021-09-30 23:39:10 +0200

    added separate bytes for OG/NFT plots, reformatted and simplified

--- farmer/get_harvester_overview_json.sh
+++ farmer/get_harvester_overview_json.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
-curl -s --insecure --cert ~/.chia/mainnet/config/ssl/farmer/private_farmer.crt --key ~/.chia/mainnet/config/ssl/farmer/private_farmer.key -d '{}' -H "Content-Type: application/json" -X POST https://localhost:8559/get_harvesters | jq -r '.harvesters[] | {(.connection.node_id): {"host": .connection.host,"port":.connection.port, "plots_overall":(.plots| length), "plots_OG":([select(.plots[].pool_public_key!=null)]| length),"plots_NFT":([select(.plots[].pool_contract_puzzle_hash!=null)]| length),"bytes_overall": ([.plots[].file_size] | reduce .[] as $item (0; . + $item)),"failed_to_open_filenames":(.failed_to_open_filenames| length), "no_key_filenames": (.no_key_filenames| length)}}'
+curl -s --insecure \
+	--cert ~/.chia/mainnet/config/ssl/farmer/private_farmer.crt \
+	--key ~/.chia/mainnet/config/ssl/farmer/private_farmer.key \
+	-H "Content-Type: application/json" \
+	-d '{}' \
+	-X POST \
+	https://localhost:8559/get_harvesters \
+		| jq -r '
+			.harvesters[] 
+				| {
+					(.connection.node_id): {
+						"host": .connection.host,
+						"port":.connection.port, 
+						"plots_overall": .plots| length, 
+						"plots_OG":  .plots | map(select(.pool_public_key)) | length,
+						"plots_NFT": .plots | map(select(.pool_contract_puzzle_hash)) | length,
+						"bytes_overall": .plots | map(.file_size) | add,
+						"bytes_OG": .plots | map(select(.pool_public_key).file_size) | add,
+						"bytes_NFT": .plots | map(select(.pool_contract_puzzle_hash).file_size) | add,
+						"failed_to_open_filenames": .failed_to_open_filenames | length, 
+						"no_key_filenames": .no_key_filenames| length
+					}
+				}
+		'
