d3eb7f856ed7d38c405c41e8a3cbe308abfb311f
neurosis69 <83925572+neurosis69@users.noreply.github.com>
2021-10-05 00:18:16 +0200

    Update get_pool_login_link_uri.sh

--- farmer/get_pool_login_link_uri.sh
+++ farmer/get_pool_login_link_uri.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source initialize.sh
-LAUNCHERID=`curl -s --insecure --cert ~/.chia/mainnet/config/ssl/wallet/private_wallet.crt --key ~/.chia/mainnet/config/ssl/wallet/private_wallet.key -d '{"wallet_id":"2"}' -H "Content-Type: applic'a'tion/json" -X POST https://$TARGETIP:9256/pw_status|jq -r '.state.launcher_id'`
+LAUNCHERID=`curl -s --insecure --cert $SSLPATH/private_wallet.crt --key $SSLPATH/private_wallet.key -d '{"wallet_id":"2"}' -H "Content-Type: applic'a'tion/json" -X POST https://$TARGETIP:9256/pw_status|jq -r '.state.launcher_id'`
 
 curl -s --insecure --cert $SSLPATH/private_farmer.crt --key $SSLPATH/private_farmer.key -d "{\"launcher_id\":\"${LAUNCHERID}\"}" -H "Content-Type: application/json" -X POST https://$TARGETIP:8559/get_pool_login_link | jq -r '.login_link'
