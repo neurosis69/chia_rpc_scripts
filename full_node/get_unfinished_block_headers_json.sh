8db092848e0a2a5b648cd6dc72ff773713a5873a
neurosis69 <83925572+neurosis69@users.noreply.github.com>
2021-10-05 00:41:16 +0200

    Update get_unfinished_block_headers_json.sh

--- full_node/get_unfinished_block_headers_json.sh
+++ full_node/get_unfinished_block_headers_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 source initialize.sh
 curl -s --insecure --cert $SSLPATH/private_full_node.crt --key $SSLPATH/private_full_node.key -d '{}' -H "Content-Type: application/json" -X POST https://$TARGETIP:8555/get_unfinished_block_headers| jq
-{\rtf1}
