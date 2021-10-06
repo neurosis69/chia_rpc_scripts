0358f87383f53498450d8d5a5d1cb1f239429fbf
neurosis69 <83925572+neurosis69@users.noreply.github.com>
2021-10-06 21:22:43 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ echo $BASEPATH
 declare -a SERVICETYPES=("farmer" "wallet" "full_node" "harvester")
 
 #read if local/removte setup
-read -p "Enter setup type (local/remote) [Default: local]:" SETUPTYPE
+read -p $'\nEnter setup type (local/remote) [Default: local]:\n' SETUPTYPE
 SETUPTYPE=${SETUPTYPE:-"local"}
 
 #exit if unknown setuptype was entered
@@ -25,10 +25,11 @@ SETUPTYPE=${SETUPTYPE:-"local"}
 } ||
 {
 	for i in  ${SERVICETYPES[@]}; do
-		echo -e "$i Configuration"
-		read -p "Enter IP address of $i: " IP
-		read -p "Enter path to SSL cert/key files for ${i} [Default: $OSUSERHOME/.chia]:" SSLPATH
-		echo -e "\n"
+		echo -e "\n###################################"
+		echo -e "#               $i                  #"
+		echo -e "#####################################"
+		read -p 'Enter IP address of $i: ' IP
+		read -p $'Enter path to SSL cert/key files for ${i} [Default: $OSUSERHOME/.chia]:\n' SSLPATH
 		SSLPATH=${SSLPATH:-$OSUSERHOME/.chia}
 		#replace IP in shell scripts
 		sed -s -i "s#_IP_#$IP#g" $BASEPATH/$i/*sh
